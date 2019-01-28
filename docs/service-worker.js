3f5cbc2fa12e76b7f4be5e78936c4b10b5f7815e
openopen <openopen114@gmail.com>
2019-01-28 21:25:54 +0800

    build docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6a9fae3c93d81d0e2f95bdef66b003b.js"
+  "/react-context-api-demo/precache-manifest.b1c9057ee971a6bfd12c428aa74c8799.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-context-api-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
