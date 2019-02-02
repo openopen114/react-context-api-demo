7c99ad4b20915de08681fb0fa42b8d8fe90095a3
openopen <openopen114@gmail.com>
2019-02-02 16:49:13 +0800

    Updates

---
+++ precache-manifest.1f1fe561c68fb551cacd2118cfb564d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-context-api-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f8f0469f63c85d86cc2",
+    "url": "/react-context-api-demo/static/js/runtime~main.8f8f0469.js"
+  },
+  {
+    "revision": "ffc7a431ef0096c3bd85",
+    "url": "/react-context-api-demo/static/js/main.ffc7a431.chunk.js"
+  },
+  {
+    "revision": "613bb75ea9861f221bfe",
+    "url": "/react-context-api-demo/static/js/1.613bb75e.chunk.js"
+  },
+  {
+    "revision": "ffc7a431ef0096c3bd85",
+    "url": "/react-context-api-demo/static/css/main.3564b9b2.chunk.css"
+  },
+  {
+    "revision": "7a015e7debf019e27f7c5804ce083d1c",
+    "url": "/react-context-api-demo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-context-api-demo/precache-manifest.1f1fe561c68fb551cacd2118cfb564d6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-context-api-demo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
